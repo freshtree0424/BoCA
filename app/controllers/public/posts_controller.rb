d541ac424d24b7ee1d934de6e8aa8c38df632cd3
freshtree0424 <srk.mizuki.kh.vat.mdg@gmail.com>
2024-04-08 01:55:42 +0000

    [Add]header,user,admin仮遷移先設定

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,2 +1,23 @@
 class Public::PostsController < ApplicationController
+
+  def new
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
