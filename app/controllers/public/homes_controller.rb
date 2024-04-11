7ad273e81dfa070f177e28b9986ada33f100f47e
freshtree0424 <srk.mizuki.kh.vat.mdg@gmail.com>
2024-04-11 12:56:07 +0000

    [Add]検索エラー対応

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,20 +1,9 @@
-class Public::HomesController < ApplicationController
+class Public::HomesController < PublicController
 
   def top
    @posts = Post.all
   end
 
   def about
-
   end
-
-  def search
-    case params[:search_model]
-    when "user" then
-      redirect_to users_path(search: params[:search])
-    when "post" then
-      redirect_to posts_path(search: params[:search])
-    end
-  end
-
 end
