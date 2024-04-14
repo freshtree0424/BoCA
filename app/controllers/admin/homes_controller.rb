1cc15789e4e304273000bb2dc971b0ced26525e0
freshtree0424 <srk.mizuki.kh.vat.mdg@gmail.com>
2024-04-14 05:29:50 +0000

    [Fix]細かい所修正

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,7 +1,12 @@
 class Admin::HomesController < AdminController
   def top
+    @posts = Post.all.order(created_at: :desc)
+    @tag_lists = {}
+    @posts.each do |post|
+      @tag_lists[post.id] = post.tags.pluck(:name).join(',')
+    end
   end
-  
+
 
   def search
     case params[:search_model]
