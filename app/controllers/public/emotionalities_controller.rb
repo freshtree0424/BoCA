c9dedf5d98b3d70371204a59a6f5d61a54cef417
freshtree0424 <srk.mizuki.kh.vat.mdg@gmail.com>
2024-04-17 14:42:48 +0000

    [Add]Ex,Emのview,controller記述追加

--- app/controllers/public/emotionalities_controller.rb
+++ app/controllers/public/emotionalities_controller.rb
@@ -3,35 +3,43 @@ class Public::EmotionalitiesController < ApplicationController
   before_action :ensure_correct_user, only: [:update]
 
   def new
-    @emotionality = Emotionality.new
+    #EmotionalityAnswerのanswerへ保存するためにnewで作成
+    @emotionality = EmotionalityAnswer.new
     @emotionalities = Emotionality.all
+    #answer保存の為の空データを作成、renderで戻ってきたときに入力済のデータを表示する
+    @answers = []
   end
 
   def create
     @emotionalities = Emotionality.all
-    @emotionalities.each do |emotionality|
-      answer = EmotionalityAnswer.new
-      answer.emotionality_id = emotionality.id # 必要に応じて質問のIDを保存
-      answer.answer = params[:emotionality][:answer] # フォームからの回答を保存
-      puts "Emotionality #{emotionality.id}: Emotionality ID #{answer.emotionality_id}"
-      if answer.save
-        flash[:success] = "Answers saved successfully!"
-        redirect_to  new_extraversion_path
-      else
-        flash[:error] = "Error saving answers."
-        render :new
-        return
-      end
+    @emotionality = EmotionalityAnswer.new
+    @answers = []
+    #eachでemotionality_answerのk(key)とv(value)をemotionality_answer_id毎に処理
+    params[:emotionality_answer].each do |k, v|
+      #@emotionality = EmotionalityAnswer.newにエラーがあればelseへ飛ぶ
+      #elseへ飛ぶバリデーションはif v.to_i == 0 で'v'が0→未回答の時
+      @emotionality.errors.add(:base, '') if v.to_i == 0
+      #k=(:emotionality_id),v=(:answer)に指定して@answerに追加
+      @answers << {
+        answer: v.to_i,
+        emotionality_id: k.to_i,
+        user_id: current_user.id,
+        created_at: Time.current,
+        updated_at: Time.current
+      }
+    end
+    #@emotionalityでエラーが無い(false)時unless以下へ、エラーがある(true)の時elseへ
+    unless @emotionality.errors.any?
+      #@answerをEmotionalityAnswerへ一括保存(insert_all)
+      EmotionalityAnswer.insert_all @answers
+      redirect_to  new_extraversion_path
+    else
+      flash[:alert] = "エラーが発生しました。未回答がないかご確認ください。"
+      render :new
     end
   end
 
   def update
-  end
-
-   private
-
-  def emotionality_params
-    params.require(:emotionality).permit(:category_id, :question,:answer)
   end
 
 end
--- app/controllers/public/extraversions_controller.rb
+++ app/controllers/public/extraversions_controller.rb
@@ -3,28 +3,43 @@ class Public::ExtraversionsController < ApplicationController
   before_action :ensure_correct_user, only: [:update]
 
   def new
-    @extraversion = Extraversion.new
+    #ExtraversionAnswerのanswerへ保存するためにnewで作成
+    @extraversion = ExtraversionAnswer.new
     @extraversions = Extraversion.all
+    #answer保存の為の空データを作成、renderで戻ってきたときに入力済のデータを表示する
+    @answers = []
   end
 
   def create
-    @extraversion = Extraversion.new(extraversion_params)
-    @extraversion.user = current_user
-    if @extraversion.save
-      redirect_to new_controllability_path
+    @extraversion = ExtraversionAnswer.new
+    @extraversions = Extraversion.all
+    @answers = []
+    #eachでextraversion_answerのk(key)とv(value)をemotionality_answer_id毎に処理
+    params[:extraversion_answer].each do |k, v|
+      #@extraversion = ExtraversionAnswer.newにエラーがあればelseへ飛ぶ
+      #elseへ飛ぶバリデーションはif v.to_i == 0 で'v'が0→未回答の時
+      @extraversion.errors.add(:base, '') if v.to_i == 0
+      #k=(:emotionality_id),v=(:answer)に指定して@answerに追加
+      @answers << {
+        answer: v.to_i,
+        extraversion_id: k.to_i,
+        user_id: current_user.id,
+        created_at: Time.current,
+        updated_at: Time.current
+      }
+    end
+    #@extraversionでエラーが無い(false)時unless以下へ、エラーがある(true)の時elseへ
+    unless @extraversion.errors.any?
+      #@answerをExtraversionAnswerへ一括保存(insert_all)
+      ExtraversionAnswer.insert_all @answers
+      redirect_to  new_controllability_path
     else
-      @extraversion = Extraversion.new
+      flash[:alert] = "エラーが発生しました。未回答がないかご確認ください。"
       render :new
     end
   end
 
   def update
-  end
-
-   private
-
-  def extraversion_params
-    params.require(:extraversion).permit(:category_id, :question)
   end
 
 end
