dd75eca7aa872f4a0b6740130f2bfa6e9b5c53d2
freshtree0424 <srk.mizuki.kh.vat.mdg@gmail.com>
2024-05-22 07:53:37 +0000

    [Add]RSpec内容追加(Post])

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,21 +1,31 @@
 require 'rails_helper'
  
-#RSpec.describe Post, "モデルに関するテスト", type: :model do
-#  describe '実際に保存してみる' do
-#    it "有効な投稿内容の場合は保存されるか" do
-#      expect(FactoryBot.build(:post)).to be_valid
-#    end
-#	  end
-#	  context "空白のバリデーションチェック" do
-#	    it "titleが空白の場合にバリデーションチェックされ空白のエラーメッセージが返ってきているか" do
-#	      post = Post.new(title: '', body:'hoge')
-#	      expect(post).to be_invalid
-#	      expect(post.errors[:title]).to include("can't be blank")
-#	    end
-#	    it "bodyが空白の場合にバリデーションチェックされ空白のエラーメッセージが返ってきているか" do
-#	      post = Post.new(title: 'hoge', body:'')
-#	      expect(post).to be_invalid
-#	      expect(post.errors[:body]).to include("can't be blank")
-#	    end
-#	  end
-#	end
+RSpec.describe Post, "Postモデルのテスト", type: :model do
+  
+  describe '実際に保存してみる' do
+    it "有効な投稿内容の場合は保存されるか" do
+      expect(FactoryBot.build(:post)).to be_valid
+    end
+
+	  context "バリデーションのテスト" do
+	    it "titleが空白の場合にバリデーションチェックされエラーメッセージが返ってきているか" do
+	      post = Post.new(title: '', body: 'hoge')
+	      expect(post).to be_invalid
+	      expect(post.errors[:title]).to include("を入力してください")
+	    end
+	    it "bodyが空白の場合にバリデーションチェックされエラーメッセージが返ってきているか" do
+	      post = Post.new(title: 'hoge', body: '')
+	      expect(post).to be_invalid
+	      expect(post.errors[:body]).to include("を入力してください")
+	    end
+	  end
+	end
+	
+	describe 'アソシエーションのテスト' do
+    context 'Userモデルとの関係' do
+      it 'N:1となっている' do
+        expect(Post.reflect_on_association(:user).macro).to eq :belongs_to
+      end
+    end
+  end
+end
