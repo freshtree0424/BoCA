c165495ab2c87b7db4e9f95fd7dd18624d2a10e1
freshtree0424 <srk.mizuki.kh.vat.mdg@gmail.com>
2024-05-23 04:45:55 +0000

    [Add]RSpec項目追加

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,30 +1,71 @@
 require 'rails_helper'
- 
-RSpec.describe Post, "Postモデルのテスト", type: :model do
-  
-  describe '実際に保存してみる' do
-    it "有効な投稿内容の場合は保存されるか" do
-      expect(FactoryBot.build(:post)).to be_valid
-    end
 
-	  context "バリデーションのテスト" do
-	    it "titleが空白の場合にバリデーションチェックされエラーメッセージが返ってきているか" do
+RSpec.describe Post, "Postモデルのテスト", type: :model do
+  describe 'バリデーションのテスト' do
+    subject { post.valid? }
+
+    let(:user) { create(:user) }
+    let!(:post) { build(:post, user_id: user.id) }
+
+    context 'titleカラム' do
+      it "titleが空白の場合にバリデーションチェックされエラーメッセージが返ってきているか" do
 	      post = Post.new(title: '', body: 'hoge')
 	      expect(post).to be_invalid
 	      expect(post.errors[:title]).to include("を入力してください")
 	    end
+    end
+
+    context 'bodyカラム' do
 	    it "bodyが空白の場合にバリデーションチェックされエラーメッセージが返ってきているか" do
 	      post = Post.new(title: 'hoge', body: '')
 	      expect(post).to be_invalid
 	      expect(post.errors[:body]).to include("を入力してください")
 	    end
+      it "201文字以上の場合にバリデーションチェックされエラーメッセージが返ってきているか" do
+        post = Post.new
+        post.body = Faker::Lorem.characters(number: 201)
+        expect(post).to be_invalid
+	      expect(post.errors[:body]).to include("は200文字以内で入力してください")
+      end
+    end
+
+    context 'starカラム' do
+    	 it "starが未選択の場合にバリデーションチェックされエラーメッセージが返ってきているか" do
+	      post = Post.new(star: '')
+	      expect(post).to be_invalid
+	      expect(post.errors[:star]).to include("を入力してください")
+	    end
 	  end
 	end
-	
+
 	describe 'アソシエーションのテスト' do
     context 'Userモデルとの関係' do
       it 'N:1となっている' do
         expect(Post.reflect_on_association(:user).macro).to eq :belongs_to
+      end
+    end
+
+    context 'PostCommentモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(Post.reflect_on_association(:post_comments).macro).to eq :has_many
+      end
+    end
+
+    context 'ViewCountモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(Post.reflect_on_association(:view_counts).macro).to eq :has_many
+      end
+    end
+
+    context 'TagRelationshipモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(Post.reflect_on_association(:tag_relationships).macro).to eq :has_many
+      end
+    end
+
+    context 'Tagモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(Post.reflect_on_association(:tags).macro).to eq :has_many
       end
     end
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,19 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe 'Userモデルのテスト', type: :model do
-
   describe 'バリデーションのテスト' do
     subject { user.valid? }
+    
     let!(:other_user) { create(:user) }
     let(:user) { build(:user) }
+    
     context 'nameカラム' do
-      it '空欄でないこと' do
+       it "nameが空白の場合にバリデーションチェックされエラーメッセージが返ってきているか" do
         user.name = ''
-        is_expected.to eq false
+        expect(user).to be_invalid
+	      expect(user.errors[:name]).to include("を入力してください")
       end
-      it '一意性がある事' do
+      it "同一のnameが既に存在する場合にバリデーションチェックされエラーメッセージが返ってきているか" do
         user.name = other_user.name
-        is_expected.to eq false
+        expect(user).to be_invalid
+	      expect(user.errors[:name]).to include("はすでに存在します")
+      end
+    end
+    
+    context 'introductionカラム' do
+      it "51文字以上の場合にバリデーションチェックされエラーメッセージが返ってきているか" do
+        user.introduction = Faker::Lorem.characters(number: 51)
+        expect(user).to be_invalid
+	      expect(user.errors[:introduction]).to include("は50文字以内で入力してください")
       end
     end
   end
@@ -22,6 +33,18 @@ RSpec.describe 'Userモデルのテスト', type: :model do
     context 'Postモデルとの関係' do
       it '1:Nとなっている' do
         expect(User.reflect_on_association(:posts).macro).to eq :has_many
+      end
+    end
+    
+    context 'PostCommentモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(User.reflect_on_association(:post_comments).macro).to eq :has_many
+      end
+    end
+    
+    context 'ViewCountモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(User.reflect_on_association(:view_counts).macro).to eq :has_many
       end
     end
   end
